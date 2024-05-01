56acfe0cfb2680432140f407183f070e18cc1121
unknown <mathis.jankovic@gmail.com>
2024-05-01 15:55:37 +0200

    Rename alias & cuontries list

--- src/fakerlocale.py
+++ src/fakerlocale.py
@@ -1,7 +1,7 @@
 from faker import Faker
-import random as r
+import random as rand
 
-_COUNTRIES = ["fr_FR", "fr_BE", "fr_CH", "fr_CA", "es_ES", "hr_HR", "it_IT", "ru_RU", "en_US", "en_GB"]
+LOCALES = ["fr_FR", "fr_BE", "fr_CH", "fr_CA", "es_ES", "hr_HR", "it_IT", "ru_RU", "en_US", "en_GB"]
 
-random = Faker(locale=r.choice(_COUNTRIES))
+random = Faker(locale=rand.choice(LOCALES))
 fr = Faker(locale="fr_FR")
